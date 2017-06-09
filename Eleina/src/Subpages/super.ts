56b7b60e004d67852ff0522e9a80404462c1a16d
AnnieCodeCrafter <ambrondijk@gmail.com>
2017-06-09 22:22:00 +0200

    working on link between js and c#

--- Eleina/src/Subpages/super.ts
+++ Eleina/src/Subpages/super.ts
@@ -1,6 +1,12 @@
 ﻿import { HttpClient, json } from "aurelia-fetch-client"
+import { autoinject } from "aurelia-framework"
+import { Router } from 'aurelia-router'
+
 
 export class Super{
+
+    
+
 
     //username = '';
     //password = '';
@@ -13,16 +19,30 @@ export class Super{
     //    this.message = 'Hello ${username}, nice to meet you.'
     //}
 
+    
+
     Username = '';
     Password = '';
     message = '';
+
+    constructor(private http: HttpClient, private router: Router) {
+
+    }
 
     sayHello() {
         this.message = `Hello ${this.Username}. Nice to meet you.`;
     }
 
     addUser() {
-        this.http.fetch{
+        this.http.fetch('login') {
+
+
+
+        }
+        
+        
+        
+
     }
 
 
